9ebc31a3724ee96d3b419516e8a2e1cffcdf68fe
LouisLesueur <louis.lesueur@gmx.fr>
2019-10-08 17:08:34 +0200

    Minor corrections

--- grid_element.py
+++ grid_element.py
@@ -1,25 +1,37 @@
 class GridElement:
     """Base class for grid_elements"""
-    def __init__(self, pos_h: int, pos_v: int, movable: bool, symbol: str):
-        self.pos_h = pos_h
-        self.pos_v = pos_v
-        self.movable = movable
-        self._symbol = symbol
+    def __init__(self, pos_h: int, pos_v: int, is_movable: bool, symbol: str):
+
+        if isinstance(pos_h, int):
+            self.pos_h = pos_h
+        else:
+            raise Exception("pos_h must be an integer !")
+
+        if isinstance(pos_v, int):
+            self.pos_v = pos_v
+        else:
+            raise Exception("pos_v must be an integer !")
+
+        self.is_movable = is_movable
+        self.symbol = symbol
 
     def move(self, direction_h: int, direction_v: int):
         """Move a grid eleemnt
         Input: direction_h = +-1 to move forward/backward horizontally
                direction_v = +-1 to """
+        if not(isinstance(direction_h, int)):
+            raise Exception("direction_h must be an integer !")
+        if not(isinstance(direction_v, int)):
+            raise Exception("direction_v must be an integer !")
         if abs(direction_h)+abs(direction_v) > 1:
             raise Exception("Can only move one case by one")
-        if not self.movable:
+        if not self.is_movable:
             raise Exception("Trying to move a static object !")
         self.pos_h += direction_h
         self.pos_v += direction_v
 
-    @property
-    def symbol(self):
-        return self._symbol
+    def __str__(self):
+        return self.symbol
 
 
 class Character(GridElement):
@@ -52,7 +64,7 @@ class Crate(GridElement):
         GridElement.__init__(self, pos_h, pos_v, True, '*')
 
 
-class EmptyCase(GridElement):
-    """An emptycase is a movable grid_element"""
+class EmptySquare(GridElement):
+    """An emptysquare is a movable grid_element"""
     def __init__(self, pos_h: int, pos_v: int):
         GridElement.__init__(self, pos_h, pos_v, True, ' ')
--- utils.py
+++ utils.py
@@ -34,19 +34,24 @@ def init(level: str):
     """Initialize the grid
        Input: absolute path to the level
        Output: The grid"""
-    main_grid = Grid(level)
-    return main_grid
+    return Grid(level)
 
 
 def play(main_grid: Grid):
     """Function which displays and updates the grid"""
+    order_symbols = {'v': (1, 0), '^': (-1, 0), '>': (0, 1), '<': (0, -1)}
     while main_grid.win == 0:
+        # Je laisse ça ici sinon on ne voit plus les messages
         main_grid.show()
-        orders = input("Move with zqsd: ")
+        orders = input("Move with <>v^: ")
         for order in orders:
-            main_grid.move(order)
+            if order in order_symbols:
+                direction_h, direction_v = order_symbols[order]
+                main_grid.move(direction_h, direction_v)
+            else:
+                print(order+' is not an acceptable character !')
 
         if main_grid.win == 1:
             print("You win !")
-        else:
+        elif main_grid == -1:
             print("You lose !")
